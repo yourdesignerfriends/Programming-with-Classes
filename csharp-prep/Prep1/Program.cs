38af10e7591f2670ef6134c270ea0740abfc887d
yourdesignerfriends <del23038@byui.edu>
2024-04-30 13:44:34 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("\nWhat is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        TextInfo myTI = new CultureInfo("en-US",false).TextInfo;
+
+        Console.Write("\nYour name is {1},", last_name, myTI.ToTitleCase( last_name ));
+        Console.Write(" {1}", name, myTI.ToTitleCase( name ));
+        Console.WriteLine(" {1}.\n", last_name, myTI.ToTitleCase( last_name ));
     }
 }
