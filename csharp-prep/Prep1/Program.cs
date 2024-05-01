16f4fbc04f8a4c31bba07bb4edf5dacccb5da6ca
yourdesignerfriends <del23038@byui.edu>
2024-05-01 14:34:21 -0700

    Preparation 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,20 +1,25 @@
 using System;
-using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        /*
+        Overview:
+        An iconic line from the James Bond movies is that he would introduce himself as "Bond, James Bond." 
+        For this assignment you will write a program that asks for your name and repeats it back in this way.
+        Assignment:
+        Prompt the user for their first name. Then, prompt them for their last name. Display the text 
+        back all on one line saying, "Your name is last-name, first-name, last-name".
+        Make sure to be precise! You should have the spacing, comma, and period appear exactly as shown in the examples.
+        */
+        
         Console.Write("\nWhat is your first name? ");
-        string name = Console.ReadLine();
+        string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
-        TextInfo myTI = new CultureInfo("en-US",false).TextInfo;
-
-        Console.Write("\nYour name is {1},", last_name, myTI.ToTitleCase( last_name ));
-        Console.Write(" {1}", name, myTI.ToTitleCase( name ));
-        Console.WriteLine(" {1}.\n", last_name, myTI.ToTitleCase( last_name ));
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
