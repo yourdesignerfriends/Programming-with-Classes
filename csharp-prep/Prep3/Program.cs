5dccaba57b94ddde8d0208fb21045cf5cf9a2725
yourdesignerfriends <del23038@byui.edu>
2024-05-02 12:48:51 -0700

    Add a comment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
+        Overview
+        In the Guess My Number game the computer picks a magic number, and then the user tries to guess it. 
+        After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.
+        This assignment is a little tricky, because it brings together many of the concepts you've learned in 
+        this course including loops and if statements.
+        */
         do
         {
             Console.Write("\nHello, I'm Analina 🙂, can you guess what number from 1 to 100 I'm thinking of 🤔... \nIf you do it you will receive a prize 🌺🌺🌺\n");
