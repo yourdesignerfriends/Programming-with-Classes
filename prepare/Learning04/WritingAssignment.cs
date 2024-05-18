9f8e15cd92e6772c595c41e7b2a622d5e6cf1bc0
yourdesignerfriends <del23038@byui.edu>
2024-05-17 20:28:17 -0700

    I corrected a comment

--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -7,8 +7,11 @@ public class WritingAssignment : BaseAssignment
     /*
     Add the member variables and set up the constructor as you did for the MathAssignment class.
     Add the GetWritingInformation() method.
-    Notice that this method needs to access the _studentName variable defined in the base class. Even though WritingAssignment class inherited this attribute, it is private, so you cannot access it directly in the derived class.
-    To get the data you need for the method you can either make the variable protected in the base class, or you can create a public GetStudentName method to return it.
+    Notice that this method needs to access the _studentName variable defined in the base class. 
+    Even though WritingAssignment class inherited this attribute, it is private, so you cannot 
+    access it directly in the derived class.
+    To get the data you need for the method you can either make the variable 
+    protected in the base class, or you can create a public GetStudentName method to return it.
     Return to Main and test your new class.
     */
     private string _title;
